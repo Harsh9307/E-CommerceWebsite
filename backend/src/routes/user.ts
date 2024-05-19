781515065b4f346d4571bb75917f04bf05f3499a
Harsh9307 <diwaseharsh@gmail.com>
2024-05-19 15:55:51 +0530

    implemented auth.js

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { newUser,getAllUsers, getUser, deleteUser } from '../controllers/user.js';
+import { adminOnly } from '../middlewares/auth.js';
 
 const app =express.Router();
 
 // route - /api/v1/user/new
 app.post("/new",newUser);
-app.get("/all",getAllUsers);
-app.route("/:id").get(getUser).get(deleteUser);
+app.get("/all",adminOnly,getAllUsers);
+app.route("/:id").get(getUser).get(adminOnly,deleteUser);
 
 export default app;
